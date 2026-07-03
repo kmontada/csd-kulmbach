e07a663e4c17c19327a3be333c79fb832a98244c
kmontada <kmontada@me.com>
2026-07-03 13:55:44 +0200

    update homepage and faq changes

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -1,31 +1,130 @@
 export const faq = [
   {
-    question: "Ist die Teilnahme kostenlos?",
+    question: "Was ist der CSD Kulmbach?",
     answer:
-      "Ja. Die Teilnahme am Christopher Street Day Kulmbach ist kostenlos und ohne Anmeldung möglich.",
+      "Der CSD ist ein intersektionaler, queerfeministischer und politischer Raum. Er richtet sich gegen Queerfeindlichkeit, Transfeindlichkeit, Sexismus, Rassismus, Antisemitismus, Ableismus, Klassismus sowie gegen weitere Formen von Diskriminierung und Unterdrückung. Die Veranstaltung ist in zwei Teile geteilt: Zunächst führt eine Demonstration durch die Kulmbacher Innenstadt. Hier steht der politische Charakter im Vordergrund. Im Anschluss an die Demo gibt es eine Aftershowparty an der Kulturschule Gößmannsreuth. Hier kann ordentlich gefeiert werden."
   },
 
   {
-    question: "Muss ich mich anmelden?",
-    answer:
-      "Nein. Komm einfach vorbei und setze gemeinsam mit uns ein Zeichen für Vielfalt und Demokratie.",
+    question: "An- und Abreise mit dem ÖPNV",
+    answer: [
+  {
+    intro: "Anreise von Bayreuth:",
+    points: [
+      "RB24 Richtung Coburg - ab: 12.37 Bayreuth Hbf, an 13.09 Kulmbach",
+      "RE32 Richtung Bamberg - ab: 13.03 Bayreuth Hbf, an 13.35 Kulmbach",
+      "Bus 378 - ab 13.06 Bayreuth Hbf, an 13.52 Kulmbach ZOB",
+    ],
+    outro: "Letzte Verbindung nach Bayreuth Hbf: RB24 - 23.37 ab Kulmbach"
   },
 
   {
-    question: "Wo startet die Demonstration?",
-    answer:
-      "Die Demonstration beginnt um 14:00 Uhr am Platz gegenüber des Bahnhofs in Kulmbach.",
+    intro: "Anreise von Bamberg/Lichtenfels/Coburg:",
+    points: [
+      "RE35 Richtung Hof - ab 12.46 Bamberg Bhf via Lichtenfels (ab 13.02), an 13.26 Kulmbach",
+      "RB25 Richtung Kronach - ab 13.04 Bamberg Bhf, Umstieg in Lichtenfels in RB24 Richtung Bayreuth - ab 13.36, an 13.59 Kulmbach",
+      "RB28 Richtung Lichtenfels - ab 12.41 Coburg Bhf, Umstieg in Lichtenfels in RE35 Richtung Hof - ab 13.08, an 13.26 Kulmbach",
+    ],
+    outro: "Letzte Verbindung nach Bamberg (via Lichtenfels): RE35 - 22.38 ab Kulmbach"
   },
 
   {
-    question: "Wie komme ich zur Aftershow-Party?",
-    answer:
-      "Nach der Demonstration fahren Shuttlebusse vom ZOB zur Kulturschule Gößmannsreuth.",
+    intro: "Anreise von Hof:",
+    points: [
+      "RB99 Richtung Neuenmarkt-Wirsberg - ab 12.00 Hof Hbf, Umstieg in Neuenmarkt Wirsberg in RB24 Richtung Coburg, ab 12.59, an 13.09 Kulmbach",
+      "RE30 Richtung Neuhaus (Pegnitz), ab 12.36 Hof Hbf, Umstieg in Trebgast in  RE32 Richtung Bamberg, ab 13.20, an 13.35 Kulmbach",
+    ],
+    outro: "Letzte Verbindung nach Hof: RB24 - 23.37 ab Kulmbach"
+  }
+]
   },
 
   {
-    question: "Kann ich den CSD unterstützen?",
+    question: "Anreise mit dem Auto - Parkmöglichkeiten",
+    answer: [
+  {
+    intro: "Kostenlose Parkplätze:",
+    points: [
+      "Parkplatz Schwedensteg (ca. 10 Minuten Fußweg zum Startpunkt der Demo)",
+      "Parkplatz Hardenbergstraße (ca. 10 Minuten Fußweg zum Startpunkt der Demo)",
+    ],
+    outro: ""
+  },
+  {
+    intro: "Kostenpflichtige Parkplätze:",
+    points: [
+      "Tiefgarage am Eku-Platz (ca. 5 Minuten Fußweg zum Startpunkt der Demo)",
+      "Parkhaus Fritz (ca. 2 Minuten Fußweg zum Startpunkt der Demo)",
+      "DB Parkplatz am Bahnhof (ca. 2 Minuten Fußweg zum Startpunkt der Demo)",
+      "Parkhaus Basteigasse (ca. 10 Minuten Fußweg zum Startpunkt der Demo)",
+    ],
+    outro: ""
+  },
+  ]
+  },
+
+  {
+    question: "Welche Regeln gelten für den Demozug in der Innenstadt?",
+    answer: [
+  {
+    intro: "Alkohol und Drogen",
+    points: [
+      "Das Mitbringen und Konsumieren von alkoholischen Getränken ist untersagt.",
+      "Das Mitbringen sowie Konsumieren von Drogen ist untersagt.",
+    ],
+  },
+  {
+    intro: "Fotografieren und Filmen",
+    points: [
+      "Das Fotografieren und Filmen während des Demozugs ist erlaubt. Aber: Nehmt Rücksicht. Fragt vorher, bevor du Menschen fotografierst oder filmst – besonders bei Nahaufnahmen",
+    ],
+    outro: ""
+  },
+  {
+    intro: "Umweltschutz",
+    points: [
+      "Das Verwenden von Pyrotechnik, Fackeln und Ähnlichem ist untersagt",
+      "Bitte lasst keinen Müll zurück.",
+      "Das Mitbringen von Glasflaschen ist nicht erlaubt. Greift hier bitte auf Mehrwegflaschen aus alternativen Materialien oder Plastikflaschen zurück",
+      "Kein Konfetti, Glitter oder andere umweltschädliche Materialien auf den Straßen",
+      "Demonstrationen sind keine Spaziergänge für Hunde. Bitte lasst Haustiere aller Art zu Hause! Assistenzhunde sind davon ausgenommen",
+    ],
+    outro: ""
+  },
+   {
+    intro: "Kleidung und Ausdruck",
+    points: [
+      "Vielfalt ist erwünscht! Ob bunt, politisch oder kreativ - solange es keine anderen Menschen herabwürdigt oder diskriminiert",
+      "Keine Symbole oder Kleidung mit extremistischen, rassistischen oder diskriminierenden Botschaften",
+      "Gesichtsverdeckende Kleidung, Masken oder Ähnliches müssen vor Beginn des Demoumzugs bei der Polizei unter Vorlage eines gültigen Ausweisdokuments angemeldet werden",
+      "Fahnen und Banner der teilnehmenden Organisationen mit eindeutigem CSD-positivem-Bezug sind erlaubt",
+      "Regelung für Fahnenstangen: Material nur aus Holz und Konststuff mit max. 1,50m Länge und 20mm Durchmesser"
+    ],
+  },
+  {
+    intro: "Barrierefreiheit",
+    points: [
+      "Achtet darauf, dass Wege und Zugänge für alle frei bleiben und achtet besonders auf Menschen mit Behinderung, Kinder, ältere Personen und Schwangere",
+    ],
+  },
+    {
+    intro: "Wägen udn Fahrzeuge",
+    points: [
+      "Demowägen sind nicht vorgesehen. Für Regelungen rund um Bollerwägen & Co. setzt euch bitte vorher mit dem CSD-Orga-Team in Verbindung (csd.kulmbach@web.de)",
+    ],
+  },
+  ]
+  },
+
+  {
+    question: "Regeln für Redebeiträge",
     answer:
-      "Ja! Wir freuen uns über Helfer:innen, Sponsor:innen und alle Menschen, die den CSD Kulmbach unterstützen möchten.",
+      "Wir demonstrieren für eine offene und bunte Gesellschaft, für queere Rechte und Gleichstellung. Bitte haltet euch an unser Thema und nutzt unsere Demonstration nicht für andere politische Themen."
+  },
+  
+    {
+    question: "Wie kann ich mithelfen?",
+    answer:
+      "Für den Tag der Demonstration suchen wir noch Ordner:innen. Diese tragen dazu bei, dass die Kundgebung für alle Teilnehmenden sicher, zugänglich und solidarisch verläuft. Sie handeln deeskalierend, ansprechbar und im Sinne der gemeinsamen politischen Werte. Auch über finanzielle Unterstützung freuen wir uns. Es besteht außerdem die Möglichkeit einen Infostand oder einen Workshop bei der Aftershow-Party in Gössmannsreuth anzubieten. Schreib uns dazu vorher eine Mail an csd.kulmbach@web.de",
   },
 ];
